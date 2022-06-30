1b89298b881870a540d5f6d021f2235c6d792b7e
Hasan Demirtaş <utsukushihito@outlook.com>
2022-06-30 11:42:49 +0300

    fix.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,100 +1,75 @@
-import com.diffplug.gradle.spotless.SpotlessPlugin
 import com.diffplug.spotless.LineEnding
 
 plugins {
   java
-  `java-library`
-  `maven-publish`
-  signing
   id("com.diffplug.spotless") version "6.7.2"
-  id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
-  id("com.github.johnrengelman.shadow") version "7.1.2" apply false
 }
 
-allprojects {
-  group = "tr.com.infumia"
+group = "tr.com.infumia"
 
-  extra["qualifiedProjectName"] = if (parent == null) {
-    "Server"
-  } else {
-    val parentName = parent!!.extra["qualifiedProjectName"].toString()
-    parentName + name[0].toUpperCase() + name.substring(1)
+java {
+  toolchain {
+    languageVersion.set(JavaLanguageVersion.of(17))
   }
 }
 
-subprojects {
-  apply<JavaPlugin>()
-  apply<JavaLibraryPlugin>()
-  apply<SpotlessPlugin>()
-
-  if (this.name.contains("api")) {
-    apply<MavenPublishPlugin>()
-    apply<SigningPlugin>()
+tasks {
+  compileJava {
+    options.encoding = Charsets.UTF_8.name()
   }
+
+  processResources {
+    duplicatesStrategy = DuplicatesStrategy.INCLUDE
+    from(project.the<SourceSetContainer>()["main"].resources.srcDirs) {
+      expand("pluginVersion" to project.version)
+      include("plugin.yml")
+    }
+  }
+
+  build {
+    dependsOn(jar)
+  }
+}
+
+repositories {
+  mavenCentral()
+  maven("https://papermc.io/repo/repository/maven-public/")
+}
+
+dependencies {
+  compileOnly(libs.paperApi)
+
+  compileOnly(libs.lombok)
+  compileOnly(libs.annotations)
+
+  annotationProcessor(libs.lombok)
+  annotationProcessor(libs.annotations)
+
+  testAnnotationProcessor(libs.lombok)
+  testAnnotationProcessor(libs.annotations)
+}
+
+spotless {
+  lineEndings = LineEnding.UNIX
+  isEnforceCheck = false
 
   java {
-    toolchain {
-      languageVersion.set(JavaLanguageVersion.of(17))
-    }
-  }
-
-  tasks {
-    compileJava {
-      options.encoding = Charsets.UTF_8.name()
-    }
-
-    jar {
-      archiveBaseName.set(project.extra["qualifiedProjectName"].toString())
-    }
-
-    build {
-      dependsOn(jar)
-    }
-  }
-
-  repositories {
-    mavenCentral()
-  }
-
-  dependencies {
-    compileOnly(libs.lombok)
-    compileOnly(libs.annotations)
-
-    annotationProcessor(libs.lombok)
-    annotationProcessor(libs.annotations)
-
-    testAnnotationProcessor(libs.lombok)
-    testAnnotationProcessor(libs.annotations)
-  }
-
-  spotless {
-    lineEndings = LineEnding.UNIX
-    isEnforceCheck = false
-
-    java {
-      importOrder()
-      removeUnusedImports()
-      endWithNewline()
-      indentWithSpaces(2)
-      trimTrailingWhitespace()
-      prettier(
-        mapOf(
-          "prettier" to "2.7.1",
-          "prettier-plugin-java" to "1.6.2"
-        )
-      ).config(
-        mapOf(
-          "parser" to "java",
-          "tabWidth" to 2,
-          "useTabs" to false
-        )
+    importOrder()
+    removeUnusedImports()
+    endWithNewline()
+    indentWithSpaces(2)
+    trimTrailingWhitespace()
+    prettier(
+      mapOf(
+        "prettier" to "2.7.1",
+        "prettier-plugin-java" to "1.6.2"
       )
-    }
-  }
-}
-
-nexusPublishing {
-  repositories {
-    sonatype()
+    ).config(
+      mapOf(
+        "parser" to "java",
+        "tabWidth" to 2,
+        "useTabs" to false
+      )
+    )
   }
 }
---
+++ src/main/java/tr/com/infumia/server/InfumiaServerPlugin.java
@@ -0,0 +1,7 @@
+package tr.com.infumia.server;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class InfumiaServerPlugin extends JavaPlugin {
+
+}

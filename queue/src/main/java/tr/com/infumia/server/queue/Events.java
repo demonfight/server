e4a6540269da3d30417c8bd446b3a529ca053c5b
Hasan Demirtaş <utsukushihito@outlook.com>
2022-07-02 17:59:14 +0300

    event filters/handlers.

--- queue/src/main/java/tr/com/infumia/server/queue/Events.java
+++ queue/src/main/java/tr/com/infumia/server/queue/Events.java
@@ -1,8 +1,10 @@
 package tr.com.infumia.server.queue;
 
-import net.minestom.server.MinecraftServer;
 import net.minestom.server.coordinate.Pos;
 import net.minestom.server.entity.GameMode;
+import net.minestom.server.event.EventFilter;
+import net.minestom.server.event.EventListener;
+import net.minestom.server.event.EventNode;
 import net.minestom.server.event.player.PlayerChatEvent;
 import net.minestom.server.event.player.PlayerLoginEvent;
 import net.minestom.server.event.player.PlayerMoveEvent;
@@ -11,24 +13,26 @@ import net.minestom.server.instance.InstanceContainer;
 import net.minestom.server.potion.Potion;
 import net.minestom.server.potion.PotionEffect;
 import org.jetbrains.annotations.NotNull;
+import tr.com.infumia.server.minestom.EventFilters;
+import tr.com.infumia.server.minestom.EventHandlers;
 
 public interface Events {
+
   static void init(@NotNull final InstanceContainer container) {
     final var spawnPoint = new Pos(0.5d, 63.0d, 0.5d);
-    final var eventHandler = MinecraftServer.getGlobalEventHandler();
-    eventHandler.addListener(
-      PlayerLoginEvent.class,
-      event -> {
+    final var login = EventListener
+      .builder(PlayerLoginEvent.class)
+      .handler(event -> {
         final var player = event.getPlayer();
         player.setFlying(true);
         player.setAutoViewable(false);
         player.setRespawnPoint(spawnPoint);
         event.setSpawningInstance(container);
-      }
-    );
-    eventHandler.addListener(
-      PlayerSpawnEvent.class,
-      event -> {
+      })
+      .build();
+    final var spawn = EventListener
+      .builder(PlayerSpawnEvent.class)
+      .handler(event -> {
         final var player = event.getPlayer();
         player.setGameMode(GameMode.ADVENTURE);
         player.setEnableRespawnScreen(false);
@@ -45,21 +49,22 @@ public interface Events {
         player.addEffect(
           new Potion(PotionEffect.SLOWNESS, Byte.MAX_VALUE, Integer.MAX_VALUE)
         );
-      }
-    );
-    eventHandler.addListener(
-      PlayerMoveEvent.class,
-      event -> {
-        if (
-          !event.getNewPosition().sameBlock(event.getPlayer().getPosition())
-        ) {
-          event.setCancelled(true);
-        }
-      }
-    );
-    eventHandler.addListener(
-      PlayerChatEvent.class,
-      event -> event.setCancelled(true)
-    );
+      })
+      .build();
+    final var move = EventListener
+      .builder(PlayerMoveEvent.class)
+      .filter(EventFilters.ignoreSameBlock())
+      .handler(EventHandlers.cancel())
+      .build();
+    final var chat = EventListener
+      .builder(PlayerChatEvent.class)
+      .handler(EventHandlers.cancel())
+      .build();
+    EventNode
+      .type("player-events", EventFilter.ALL.PLAYER)
+      .addListener(login)
+      .addListener(spawn)
+      .addListener(move)
+      .addListener(chat);
   }
 }

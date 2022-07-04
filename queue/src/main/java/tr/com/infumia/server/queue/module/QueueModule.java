037b0a5ccfb931122d7809e8f9d53ec8be6a6079
Hasan Demirtaş <utsukushihito@outlook.com>
2022-07-04 13:28:22 +0300

    Update QueueModule.java

--- queue/src/main/java/tr/com/infumia/server/queue/module/QueueModule.java
+++ queue/src/main/java/tr/com/infumia/server/queue/module/QueueModule.java
@@ -1,42 +1,45 @@
 package tr.com.infumia.server.queue.module;
 
-import java.time.Duration;
 import java.util.stream.Collectors;
 import net.minestom.server.MinecraftServer;
 import net.minestom.server.entity.Entity;
+import net.minestom.server.timer.ExecutionType;
+import net.minestom.server.timer.TaskSchedule;
 import org.jetbrains.annotations.NotNull;
 import tr.com.infumia.server.common.Redis;
 import tr.com.infumia.terminable.TerminableConsumer;
 import tr.com.infumia.terminable.TerminableModule;
 
 public record QueueModule() implements TerminableModule {
-  private static final String QUEUE_MODE_KEY = "queue-mode";
-
   @Override
   public void setup(@NotNull final TerminableConsumer consumer) {
+    final var queueModeKey = "queue-mode";
     final var pool = Redis.connectionPool();
+    final Runnable task = () -> {
+      final var players = MinecraftServer
+        .getConnectionManager()
+        .getOnlinePlayers();
+      final var uuids = players
+        .stream()
+        .map(Entity::getUuid)
+        .map(Object::toString)
+        .collect(Collectors.toSet());
+      final var modes = pool
+        .acquire()
+        .thenCompose(connection -> {
+          final var map = connection.sync().hgetall(queueModeKey);
+          return pool.release(connection).thenApply(unused -> map);
+        })
+        .join();
+    };
     final var schedule = MinecraftServer
       .getSchedulerManager()
-      .buildTask(() -> {
-        final var players = MinecraftServer
-          .getConnectionManager()
-          .getOnlinePlayers()
-          .stream()
-          .map(Entity::getUuid)
-          .map(Object::toString)
-          .collect(Collectors.toSet());
-        final var modes = pool
-          .acquire()
-          .thenCompose(connection -> {
-            final var map = connection
-              .sync()
-              .hgetall(QueueModule.QUEUE_MODE_KEY);
-            return pool.release(connection).thenApply(unused -> map);
-          })
-          .join();
-      })
-      .repeat(Duration.ofSeconds(1L))
-      .schedule();
+      .scheduleTask(
+        task,
+        TaskSchedule.immediate(),
+        TaskSchedule.seconds(1L),
+        ExecutionType.ASYNC
+      );
     consumer.bind(schedule::cancel);
   }
 }

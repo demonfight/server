10e368c861ce0cfb5e8b147956c49bf8c618bc09
Hasan Demirtaş <utsukushihito@outlook.com>
2022-07-05 12:00:36 +0300

    Update ServiceQueue.java

---
+++ common/src/main/java/tr/com/infumia/server/common/ServiceQueue.java
@@ -0,0 +1,62 @@
+package tr.com.infumia.server.common;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.Accessors;
+import lombok.experimental.FieldDefaults;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * an interface to determine service queues.
+ */
+public interface ServiceQueue {
+  /**
+   * initiates the queue for services.
+   *
+   * @param current the current service dns to initiate.
+   * @param target the target service dns to initiate.
+   */
+  @NotNull
+  static Impl init(
+    @NotNull final String current,
+    @NotNull final String target
+  ) {
+    return new Impl(current, target);
+  }
+
+  /**
+   * obtains the current.
+   *
+   * @return current.
+   */
+  @NotNull
+  String current();
+
+  /**
+   * obtains the target.
+   *
+   * @return target.
+   */
+  @NotNull
+  String target();
+
+  @Getter
+  @RequiredArgsConstructor
+  @Accessors(fluent = true)
+  @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+  final class Impl implements ServiceQueue {
+
+    /**
+     * the current.
+     */
+    @NotNull
+    String current;
+
+    /**
+     * the target.
+     */
+    @NotNull
+    String target;
+  }
+}

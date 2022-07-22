112c612ef5c5f41fdbf9ea463b6b6e5aca39bce6
Hasan Demirtaş <utsukushihito@outlook.com>
2022-07-22 22:23:36 +0300

    Update Server.java

--- queue/src/main/java/com/demonfight/server/queue/Server.java
+++ queue/src/main/java/com/demonfight/server/queue/Server.java
@@ -1,9 +1,7 @@
 package com.demonfight.server.queue;
 
 import com.demonfight.server.minestom.Servers;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 public final class Server {
 
   public static void main(final String[] args) {
